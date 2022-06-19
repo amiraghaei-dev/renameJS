19d0d3f0f4e038019844be3af4ce1269bfb971cc
Amir aghayi <ujkgmt@gmail.com>
2022-06-19 07:02:25 +0430

    add mkdir fn and move files

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const yargs = require("yargs");
 const chalk = require("chalk");
-const { renameFN } = require("./modules");
+const { renameFN, moveFN } = require("./modules");
 
-// using ===>  node ./app.js rename p --path C:\Users\ujkgm\OneDrive\Desktop\renameJS\title t --text _Downloadly.ir_en
 
 yargs.scriptName(`${chalk.yellow("rename files")}`);
 yargs.usage(`$0 ${chalk.red("<command>")} ${chalk.green("[args]")}`);
 
 yargs.version("1.1.0");
 
+// using ===>  node ./app.js rename p --path C:\Users\ujkgm\OneDrive\Desktop\renameJS\title t --text _Downloadly.ir_en
 yargs.command({
   command: "rename",
   aliases: ["r"],
@@ -29,6 +29,30 @@ yargs.command({
   },
   handler({ path, text }) {
     renameFN(path, text);
+  },
+});
+
+// using ====> node ./app.js move p --path C:\Users\ujkgm\OneDrive\Desktop\renameJS\title f --folder subTitle
+yargs.command({
+  command: "move",
+  aliases: ["m"],
+  describe: `${chalk.green("[move Files]")}`,
+  builder: {
+    path: {
+      alias: "p",
+      describe: "A folder where you can search and rename files",
+      demandOption: true,
+      type: "string",
+    },
+    path: {
+      alias: "f",
+      describe: "Text to remove from file name",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler({ path, folder }) {
+    moveFN(path, folder);
   },
 });
 
--- modules.js
+++ modules.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const chalk = require("chalk");
+const pathNode = require("path");
+
 const {
   promises: { readdir },
 } = require("fs");
@@ -8,7 +10,7 @@ const renameFN = (path, text) => {
   console.log(path, text);
   const files = fs.readdirSync(path);
 
-//*   rename files in the sub folder of the root folder
+  //*   rename files in the sub folder of the root folder
 
   const getDirectories = async (source) =>
     (await readdir(source, { withFileTypes: true }))
@@ -42,9 +44,7 @@ const renameFN = (path, text) => {
     }
   });
 
-
-
-//*   rename files in the root of the folder
+  //*   rename files in the root of the folder
 
   if (files.length <= 0) {
     console.log(chalk.red("not found files"));
@@ -64,4 +64,78 @@ const renameFN = (path, text) => {
   }
 };
 
-module.exports = { renameFN };
+const moveFN = (path, folderName) => {
+  // console.log(path, folder);
+  const files = fs.readdirSync(path);
+
+  //*   rename files in the sub folder of the root folder
+
+  const getDirectories = async (source) =>
+    (await readdir(source, { withFileTypes: true }))
+      .filter((dirent) => dirent.isDirectory())
+      .map((dirent) => dirent.name);
+
+  getDirectories(path).then((folders) => {
+    console.log(folders);
+    // fs.mkdirSync(__dirname + "/" + "title" + "/" + "amir", true);
+    for (const folder of folders) {
+      fs.mkdirSync(
+        path + '/' + folder + "/" + folderName,
+        { recursive: true },
+        (err) => {
+          console.log(err)
+        }
+      )
+      const filesDir = fs.readdirSync(path + "/" + folder);
+      for (const file of filesDir) {
+        const d = new Date();
+        console.log(
+          chalk.green(`[${chalk.green(d.toLocaleString())}]`),
+          `${path}/${folder}/${file}`,
+          `[${chalk.yellow(`successful`)}]`
+        );
+        if (file.endsWith(".srt")) {
+          fs.renameSync(
+            `${path}/${folder}/${file}`,
+            `${path}/${folder}/${folderName}/${file}`,
+            (err) => {
+              console.log(err);
+            }
+          );
+        }
+      }
+      console.log(
+        chalk.yellow("(folder)"),
+        chalk.green(`${folder} [successful]`)
+      );
+    }
+  });
+
+  //*   rename files in the root of the folder
+
+  if (files.length <= 0) {
+    console.log(chalk.red("not found files"));
+  } else {
+    fs.mkdirSync(
+      path + "/" + folderName,
+      { recursive: true },
+      (err) => {
+        console.log(err)
+      }
+    )
+    for (const file of files) {
+      if (file.endsWith(".srt")) {
+        fs.renameSync(
+          path + "/" + file,
+          path + "/"+ folderName + "/" + file,
+          (err) => {
+            console.log(err);
+          }
+        );
+      }
+    }
+    console.log(chalk.green("successful"));
+  }
+};
+
+module.exports = { renameFN, moveFN };
